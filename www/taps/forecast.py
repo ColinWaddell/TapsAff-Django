79c086ce667a41974368f712bd1e3c41170ba06d
Colin Waddell <mrcolin@gmail.com>
2019-03-20 17:58:16 +0000

    another place error catch

--- www/taps/forecast.py
+++ www/taps/forecast.py
@@ -107,7 +107,7 @@ def _build_packet():
 def _build_forecast(packet, raw):
     # Test if we've got a valid location
     try:
-        if not raw["location"]:
+        if not raw["location"] or not raw["current_observation"]:
             raise TapsLocationError()
     except KeyError:
         raise TapsRequestError("Bad data returned from weather service.")
