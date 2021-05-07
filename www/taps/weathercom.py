de5262d15545078ee3e5843e49fd5d20f3c861ee
Colin Waddell <mrcolin@gmail.com>
2021-05-07 21:51:27 +0100

    weather getter

---
+++ www/taps/weathercom.py
@@ -0,0 +1,20 @@
+import urllib.request, json
+
+
+def build_url(location, api_key):
+    return (
+        "http://api.weatherapi.com/v1/forecast.json?"
+        f"key={api_key}&"
+        f"q={location}&"
+        "days=7&"
+        "aqi=no&"
+        "alerts=no"
+    )
+
+
+def get_weather(location, api_key):
+    data = None
+    with urllib.request.urlopen(build_url(location, api_key)) as url:
+        data = json.loads(url.read().decode())
+
+    return data
